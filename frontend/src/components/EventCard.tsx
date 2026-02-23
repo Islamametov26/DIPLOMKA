ca06386b4a6ef6c01d871704f3db9e6e218b9612
Alexey Ovchinnikov <alexey.ovchinnikov.dev@gmail.com>
2026-02-23 16:05:59 +0500

    Improve event cards layout and remove published label

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -33,7 +33,6 @@ function EventCard({ event, venueName, onDetails }: Props) {
         <div className="event-card__image event-card__image--placeholder" aria-hidden="true" />
       )}
       <div className="event-card__header">
-        <span className="event-card__tag">{event.published ? 'Опубликовано' : 'Черновик'}</span>
         <span className="event-card__time">{formatRange(event)}</span>
       </div>
       <h3 className="event-card__title">{safeTitle}</h3>
@@ -41,9 +40,11 @@ function EventCard({ event, venueName, onDetails }: Props) {
       <div className="event-card__footer">
         <span className="event-card__meta">Площадка: {safeVenue}</span>
       </div>
-      <button className="event-card__hover-action" type="button" onClick={() => onDetails(event)}>
-        Купить билет
-      </button>
+      <div className="event-card__actions">
+        <button className="event-card__hover-action" type="button" onClick={() => onDetails(event)}>
+          Купить билет
+        </button>
+      </div>
     </article>
   )
 }
